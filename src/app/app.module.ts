669d2ae944c879e0ce0a49f4ff07267e523fa0ad
huyloc07104 <locthph07104@fpt.edu.vn>
2020-03-28 17:48:00 +0700

    3 files modified, 9 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { SlidehomeComponent } from './slidehome/slidehome.component';
+import { ListproductComponent } from './listproduct/listproduct.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, SlidehomeComponent, ListproductComponent, FooterComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
